7644ba9bb29327085bc8a76a9a750471a33a1bbd
Guilherme Colli <guicolli.colli@gmail.com>
2025-11-06 16:27:42 -0300

    animações

--- js/depoimento.js
+++ js/depoimento.js
@@ -1,29 +1,24 @@
-// Ela recebe uma nota (ex: 4) e retorna o HTML das estrelas
 function gerarEstrelasHTML(nota) {
-    let estrelasHTML = ''; // Começa com uma string vazia
-    const maxEstrelas = 5; // Definimos o total de estrelas
+    let estrelasHTML = '';
+    const maxEstrelas = 5;
 
-    // Loop 1: Adiciona as estrelas CHEIAS
     for (let i = 0; i < nota; i++) {
         estrelasHTML += '<span>&#9733;</span>'; 
     }
 
-    // Loop 2: Adiciona as estrelas VAZIAS
-    // Se a nota for 3, ele vai rodar para i = 3 e i = 4 (total de 2 vazias)
     for (let i = nota; i < maxEstrelas; i++) {
         estrelasHTML += '<span>&#9734;</span>';
     }
 
-    return estrelasHTML; // Retorna o HTML pronto
+    return estrelasHTML;
 }
 
-// Espera a página carregar
 document.addEventListener("DOMContentLoaded", function() {
-    // 1. Pega o constainer-depoimentos, onde serão inseridos os cards
+
     const container = document.getElementById("container-depoimentos");
-    // 2. Loop: para cada depoimento dentro do array depoimentos
+
     depoimentos.forEach(function(depoimento) {
-        // 3. Cria o "Molde" HTML
+
         const cardHTML = `
         <article class="card-depoimento">
             <header>
@@ -43,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 <p>${depoimento.texto}</p>
             </blockquote>
         </article>`;
-        // 4. Insere o HTML recém-criado dentro do container
+
         container.innerHTML += cardHTML;
     });
 });
